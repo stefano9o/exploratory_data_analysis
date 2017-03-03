ab18dee4c7bd392451d3e4442c5b9349e0040146
stefano9o <stefano.galeano@gmail.com>
2017-03-03 13:07:36 +0100

    fix comment on plot6.R

--- plot6.R
+++ plot6.R
@@ -1,9 +1,9 @@
 
 ##
 ## Question:
-## Across the United States, how have emissions from coal combustion-related sources changed 
-## from 1999-2008?
-##  
+## Compare emissions from motor vehicle sources in Baltimore City with emissions from motor 
+## vehicle sources in Los Angeles County, California (fips == "06037"). Which city has seen 
+## greater changes over time in motor vehicle emissions?
 ##
 
 library(ggplot2)
